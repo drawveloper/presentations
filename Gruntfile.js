cd0812215c91930040bfe1a34aa6aa4a92ab62d7
Guilherme Rodrigues <guilherme@vtex.com.br>
2013-03-12 14:28:42 -0300

    Offline fonts and livereload with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+'use strict';
+var path = require('path');
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+
+var folderMount = function folderMount(connect, point) {
+  return connect.static(path.resolve(point));
+};
+
+module.exports = function (grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    connect: {
+      livereload: {
+        options: {
+          port: 9001,
+          middleware: function(connect, options) {
+            return [lrSnippet, folderMount(connect, '.')]
+          }
+        }
+      }
+    },
+    regarde: {
+      html: {
+        files: '**/*.html',
+        tasks: ['livereload']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-regarde');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+
+  grunt.registerTask('default', ['livereload-start', 'connect', 'regarde']);
+};
