5f3e8c54b0bbbcefbd84630691d257577e3c70a3
Guilherme Rodrigues <gadr90@gmail.com>
2013-12-09 19:39:01 -0200

    Update to v2

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+/* global module:false */
+module.exports = function(grunt) {
+	var port = grunt.option('port') || 8000;
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		connect: {
+			server: {
+				options: {
+					host: '*',
+					port: port,
+					base: '.'
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: true
+			},
+			main: {
+				files: [ 'css/main.css', '**/*.html' ]
+			}
+		}
+
+	});
+
+	// Dependencies
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+
+	// Default task
+	grunt.registerTask( 'default', [ 'connect', 'watch' ] );
+};
